f357724d75fa7009cff57ab2e8ade61bf4e3b4ad
CharlieBrownyc <brownyc6@gmail.com>
2025-07-10 04:59:51 +0900

    add grpc-multimodule-project prototype

---
+++ grpc-server-multimod/grpc-rdb/src/main/java/kr/co/brownyc/rdb/GrpcRdbApplication.java
@@ -0,0 +1,13 @@
+package kr.co.brownyc.rdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcRdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcRdbApplication.class, args);
+    }
+
+}
